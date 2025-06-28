f0358e41c7f2f9d7cd8c93df01a35fede54432ca
SteveStarks01 <91699436+SteveStarks01@users.noreply.github.com>
2025-06-28 14:09:21 +0100

    Fix headers called outside request scope error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
 
+export const dynamic = 'force-dynamic';
+
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <main className="font-work-sans">
